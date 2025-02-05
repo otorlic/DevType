de00a502fcef687e86f7b3d9cd3588e13cfb6803
oorlic14 <165859762+oorlic14@users.noreply.github.com>
2025-02-05 13:36:33 -0800

    added longer code chunk

--- typing.js
+++ typing.js
@@ -1,14 +1,12 @@
 
 const code = [
-    "for (int i = 0; i < 200; i++) {\n    buffer[i] = 0;\n}",
+    "for (int i = 0; i < 200; i++) {\n\tbuffer[i] = 0;\n}",
     "System.out.println(\"Hello, World!\");",
-    "public void absorb(byte[] data, int len) {\n    this.absorb(data, 0, len);\n}"
+    "public void absorb(byte[] data, int len) {\n\tthis.absorb(data, 0, len);\n}",
+    "if (percent >= 90) {\n\tSystem.out.println(\"You got an A!\");\n} else if (percent >= 80) {\n\tSystem.out.println(\"You got a B!\");\n} else if (percent >= 70) {\n\tSystem.out.println(\"You got a C!\");\n} else if (percent >= 60) {\n\tSystem.out.println(\"You got a D!\");\n} else {\n\tSystem.out.println(\"You got an F!\");\n}"
 ];
 
 function codeChooser() {
     const index = Math.floor(Math.random() * code.length);
     document.getElementById("codeChunk").innerHTML = code[index];
-
-    //const chunk = document.getElementById("test");
-    //chunk.innerHTML = '<h1 id="codeChunk">System.out.println("Hello, World!");</h1>';
 }

ca143045dd18787dd21f8ecca5eb60f758d7f1b5
oorlic14 <165859762+oorlic14@users.noreply.github.com>
2025-02-11 01:55:48 -0800

    letters pop up when you type but you can't see what to type.

--- typing.js
+++ typing.js
@@ -1,3 +1,5 @@
+let index = 0;
+let i = 0;
 
 const code = [
     "for (int i = 0; i < 200; i++) {\n\tbuffer[i] = 0;\n}",
@@ -6,7 +8,24 @@ const code = [
     "if (percent >= 90) {\n\tSystem.out.println(\"You got an A!\");\n} else if (percent >= 80) {\n\tSystem.out.println(\"You got a B!\");\n} else if (percent >= 70) {\n\tSystem.out.println(\"You got a C!\");\n} else if (percent >= 60) {\n\tSystem.out.println(\"You got a D!\");\n} else {\n\tSystem.out.println(\"You got an F!\");\n}"
 ];
 
+let chunk = code[index];
+let currentChar = 0;
 function codeChooser() {
-    const index = Math.floor(Math.random() * code.length);
-    document.getElementById("codeChunk").innerHTML = code[index];
+    index = Math.floor(Math.random() * code.length);
+    document.getElementById("codeChunk").innerHTML = "";
+    chunk = code[index];
+    currentChar = 0;
+}
+
+
+
+document.body.addEventListener("keypress", type);
+
+
+function type() {
+    
+    let letter = chunk.charAt(currentChar);
+    document.getElementById("codeChunk").innerHTML += letter;
+    currentChar++;
+
 }
